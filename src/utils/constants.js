7ad471ff69cad13ef52f5574d17131eb424dc8af
Ritesh <riteshjha24060@gmail.com>
2025-11-10 17:28:50 +0530

    Stored API Keys

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const Google_API_KEY = AIzaSyBlX9dl8o6h2LZ - Hu50mzyx4yWJd3om4dA;
+
+export const YOUTUBE_API_KEY =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  Google_API_KEY;
