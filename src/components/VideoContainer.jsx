ec1adf107c6a3af7420173d0eb01a280960f3acc
Ritesh <riteshjha24060@gmail.com>
2025-11-11 17:06:07 +0530

    Setup video card routing

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_API_KEY } from '../utils/constants'
 import VideoCard from './VideoCard'
+import { Link } from 'react-router'
 
 const VideoContainer = () => {
   const [videos, setvideos] = useState([])
@@ -18,7 +19,7 @@ const VideoContainer = () => {
   return (
     <div className="flex flex-wrap gap-4 p-4">
       {videos.map((video) => (
-        <VideoCard key={video.id} info={video}></VideoCard>
+        <Link to={"/watch"}><VideoCard key={video.id} info={video}></VideoCard></Link>
       ))}
     </div>
   );
